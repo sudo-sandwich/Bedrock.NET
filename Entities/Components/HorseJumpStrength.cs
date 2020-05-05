6784918471cd0cb4636cad18380cd2d369594982
Misys <luskconrad@gmail.com>
2020-05-04 21:15:03 -0400

    added horse stuff

---
+++ Entities/Components/HorseJumpStrength.cs
@@ -0,0 +1,25 @@
+﻿using Bedrock.Utility;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bedrock.Entities.Components {
+    public class HorseJumpStrength : IComponent {
+        public string Name {
+            get {
+                return "minecraft:horse.jump_strength";
+            }
+        }
+
+        public double Value { get; set; }
+
+        public JProperty Generate() {
+            JObject jObject = new JObject();
+
+            jObject.AddIfNotNull("value", Value);
+
+            return new JProperty(Name, jObject);
+        }
+    }
+}
