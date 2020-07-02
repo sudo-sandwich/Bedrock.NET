9d63c46931cb0d1d2b2851b400799810fa5ee6aa
Misys <luskconrad@gmail.com>
2020-07-02 15:12:13 -0400

    added stopsound command

---
+++ Functions/Commands/StopSound.cs
@@ -0,0 +1,20 @@
+﻿using Bedrock.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bedrock.Functions.Commands {
+    public class StopSound : Command {
+        public TargetSelector Target { get; private set; }
+        public string Sound { get; private set; }
+
+        public StopSound(TargetSelector target, string sound) {
+            Target = target;
+            Sound = sound;
+        }
+
+        public override string ToString() {
+            return CommandHelper.Build("stopsound", Target, Sound);
+        }
+    }
+}
